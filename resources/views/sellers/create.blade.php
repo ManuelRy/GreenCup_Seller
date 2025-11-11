c41bf331707d806688d0aefea15560737a692a9f
ManuelRy <manuelsokleng99@gmail.com>
2025-11-11 12:02:43 +0700

    unncessary data removed

--- resources/views/sellers/create.blade.php
+++ resources/views/sellers/create.blade.php
@@ -6,7 +6,7 @@
   <title>Register Business - GreenCup</title>
   <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" 
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 
